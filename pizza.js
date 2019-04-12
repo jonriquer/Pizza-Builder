a96e3ba2d04b708276134d9b1eacacb1763982b2
Jonathan Riquer <jonriquer@gmail.com>
2019-04-12 15:15:59 -0400

    adding active class styling to buttons

---
+++ pizza.js
@@ -0,0 +1,39 @@
+function renderEverything() {
+  renderPepperonni();
+  renderMushrooms();
+  renderGreenPeppers();
+  renderWhiteSauce();
+  renderCrust();
+}
+
+function renderPepperonni() {
+  $(".btn-pepperonni").click(function(){
+    $(this).toggleClass('active');
+  });
+}
+
+function renderMushrooms() {
+  $('.btn-mushrooms').click(function(){
+    $(this).toggleClass('active');
+  })
+}
+
+function renderGreenPeppers() {
+  $('.btn-green-peppers').click(function(){
+    $(this).toggleClass('active');
+  })
+}
+
+function renderWhiteSauce() {
+  $('.btn-sauce').click(function(){
+    $(this).toggleClass('active');
+  })
+}
+
+function renderCrust() {
+  $('.btn-crust').click(function(){
+    $(this).toggleClass('active');
+  })
+}
+
+renderEverything();
